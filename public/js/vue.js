261f719aa1f507c2ec45c874f3f88f3994d3c64f
thanked <officialtrum@gmail.com>
2021-04-13 01:13:41 +0200

    Clock and date

--- public/js/vue.js
+++ public/js/vue.js
@@ -1,6 +1,28 @@
-var app = new Vue({
-    el: '#clock',
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let app = new Vue({
+    el: '#app',
     data: {
-      clock: '12:09'
+        time: null,
+        date: null,
+        state: false
+    },
+    methods: {
+        isNight() {
+            let hours = (new Date()).getHours();
+            return false;
+        }
+    },
+    beforeDestroy() {
+        clearInterval(this.interval)
+    },    
+    created() {
+        setTimeout(() => this.state = true, 1000);
+
+        this.interval = setInterval(() => {
+            this.time = Intl.DateTimeFormat('en-SE', { timeStyle: 'long' }).format(new Date()).slice(0, -5);
+            this.date = Intl.DateTimeFormat('en-SE', { dateStyle: 'full' }).format(new Date());
+        }, 1000)
     }
 });
