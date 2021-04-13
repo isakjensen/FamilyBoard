b2c4650a230fdeead7eae6c1f348107dc7a4c621
isakjensen <isakludvigjensen@gmail.com>
2021-04-13 13:03:01 +0200

    Grid layout

--- public/js/vue.js
+++ public/js/vue.js
@@ -6,20 +6,11 @@ let app = new Vue({
     data: {
         time: null,
         date: null,
-        state: false
-    },
-    methods: {
-        isNight() {
-            let hours = (new Date()).getHours();
-            return false;
-        }
     },
     beforeDestroy() {
         clearInterval(this.interval)
     },    
     created() {
-        setTimeout(() => this.state = true, 1000);
-
         this.interval = setInterval(() => {
             this.time = Intl.DateTimeFormat('en-SE', { timeStyle: 'long' }).format(new Date()).slice(0, -5);
             this.date = Intl.DateTimeFormat('en-SE', { dateStyle: 'full' }).format(new Date());
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
       colors: {
         darkGray: colors.trueGray,
         cyan: colors.cyan,
+        boxGray: '#191919'
       },
+      fontSize: {
+        'full': '100%'
+      },
+      borderWidth: {
+        '3': '3px',
+        '5': '5px',
+        '6': '6px',
+        '7': '7px'
+      } 
     },
   },
   variants: {
