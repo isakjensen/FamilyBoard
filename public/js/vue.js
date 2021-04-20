9ee55c516a9dc4d0ff8367d6d45d0309b63837ec
isakjensen <isakludvigjensen@gmail.com>
2021-04-20 14:56:43 +0200

    Added clock color method

--- public/js/vue.js
+++ public/js/vue.js
@@ -8,28 +8,29 @@ let app = new Vue({
         date: null,
         color: null
     },
-    beforeDestroy() {
-        clearInterval(this.interval)
-    },    
+    methods: {
+        GetClockColor: function() {
+            let h = new Date().getHours();
+
+            if(h >= 0 && h < 7) {
+                return 'from-indigo-600 to-purple-900'
+            } else if(h >= 7 && h < 12) {
+                return 'from-yellow-400 to-yellow-600'
+            } else if(h >= 12 && h < 19) {
+                return 'from-blue-300 to-blue-500'
+            } else if(h >= 19 && h < 22) {
+                return 'from-blue-400 to-blue-600'
+            } else if(h >= 22) {
+                return 'from-pink-600 to-purple-900'
+            }
+        }
+    },   
     created() {
         this.interval = setInterval(() => {
             this.time = Intl.DateTimeFormat('en-SE', { timeStyle: 'long' }).format(new Date()).slice(0, -5);
             this.date = Intl.DateTimeFormat('en-SE', { dateStyle: 'full' }).format(new Date());
 
-        let hours = new Date().getHours();
-
-        if(hours >= 0 && hours < 7) {
-            this.color = 'from-indigo-600 to-purple-900'
-        } else if(hours >= 7 && hours < 12) {
-            this.color = 'from-yellow-400 to-yellow-600'
-        } else if(hours >= 12 && hours < 19) {
-            this.color = 'from-blue-300 to-blue-500'
-        } else if(hours >= 19 && hours < 22) {
-            this.color = 'from-blue-400 to-blue-600'
-        } else if(hours >= 22) {
-            this.color = 'from-pink-600 to-purple-900'
-        }
-
+            this.color = this.GetClockColor();
         }, 1000);
     }
 });
